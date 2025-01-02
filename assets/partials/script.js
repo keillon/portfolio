c9c738b2e5bb94f08088c445197f51d034886f0d
unknown <keillon_sil@hotmail.com>
2025-01-01 23:12:15 -0300

    Modificado header, corrigido cores, corrigido texto dos icones, atualizado acessibilidade, mais animações e adição de salvar o tema do usuario quando recarregar a página

--- assets/partials/script.js
+++ assets/partials/script.js
@@ -46,11 +46,9 @@ sr.reveal('.card', { rotate: { x: 20, y: 50, z: 0 }, duration: 4200 })
 sr.reveal('.text-projects', { rotate: { x: 20, y: 50, z: 0 }, duration: 4200 })
 sr.reveal('.fz-5', { rotate: { x: 20, y: 50, z: 0 }, duration: 4500 })
 sr.reveal('.text__skills', { rotate: { x: 20, y: 60, z: 0 }, duration: 4200 })
-
 // transitions JS final
 
 // light-mode
-
 let trilho = document.getElementById('mode')
 let body = document.getElementById('body')
 
@@ -63,6 +61,8 @@ function saveSchemePreferences () {
   }
 }
 // função para salvar o tema no localStorage
+
+// função para carregar o tema salvo no localStorage
 function loadScheme () {
   const scheme = localStorage.getItem('scheme')
   if (scheme === 'light') {
@@ -73,14 +73,18 @@ function loadScheme () {
     trilho.classList.remove('light')
   }
 }
-// função para salvar o tema no localStorage
+// função para carregar o tema salvo no localStorage
 
+// função para alternar o tema
 trilho.addEventListener('click', () => {
   trilho.classList.toggle('light')
   body.classList.toggle('light')
   saveSchemePreferences()
 })
+// função para alternar o tema
 
+// chamar o carregar o tema
 window.addEventListener('load', loadScheme)
+// chamar o carregar o tema
 
 // light-mode final
